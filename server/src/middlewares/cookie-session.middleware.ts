64866deb7a3250f32e95e5a82b812a8dce1842dc
Ghost Order <kodaichi.kun@gmail.com>
2023-02-06 18:46:41 -0500

    secure option in cookie-session was removed

--- server/src/middlewares/cookie-session.middleware.ts
+++ server/src/middlewares/cookie-session.middleware.ts
@@ -1,7 +1,7 @@
 export const cookieSessionOptions: CookieSessionInterfaces.CookieSessionOptions = {
   name: 'session',
   sameSite: 'none',
-  secure: true,
+  // secure: true,
   // secureProxy: true,
   maxAge: 24 * 60 * 60 * 1000,
   keys: (process.env.COOKIE_SESSION_KEY_1 && process.env.COOKIE_SESSION_KEY_2) ? [ process.env.COOKIE_SESSION_KEY_1, process.env.COOKIE_SESSION_KEY_2 ] : undefined
