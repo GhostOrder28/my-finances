cf11463ea6c98c102eb0b8aef22de643a73ebd3d
Ghost Order <kodaichi.kun@gmail.com>
2023-02-07 09:38:11 -0500

    cookie-session options secure and sameSite were disabled

--- server/build/app.js
+++ server/build/app.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import path from "path";
 import morgan from "morgan";
 import passport from "passport";
@@ -13,11 +14,11 @@ import authRouter from "./routes/auth/auth.router.js";
 import usersRouter from "./routes/users/users.router.js";
 import clientsRouter from "./routes/clients/clients.router.js";
 const { __dirname } = fileDirName(import.meta);
-// const corsOptions = {
-//   origin: 'https://192.168.100.3:8080',
-//   // origin: 'https://localhost:8080',
-//   credentials: true,
-// };
+const corsOptions = {
+    origin: 'https://192.168.100.3:8080',
+    // origin: 'https://localhost:8080',
+    credentials: true,
+};
 const app = express();
 passport.use(new LocalStrategy(AUTH_OPTIONS, verifyCallback));
 passport.serializeUser((userData, done) => {
@@ -30,7 +31,7 @@ passport.deserializeUser((userId, done) => {
     done(null, userId);
 });
 app.use(morgan('combined'));
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(cookieSession(cookieSessionOptions));
 app.use(passport.initialize());
 app.use(passport.session());
--- server/build/middlewares/cookie-session.middleware.js
+++ server/build/middlewares/cookie-session.middleware.js
@@ -1,7 +1,7 @@
 export const cookieSessionOptions = {
     name: 'session',
-    sameSite: 'none',
-    secure: true,
+    // sameSite: 'none',
+    // secure: true,
     // secureProxy: true,
     maxAge: 24 * 60 * 60 * 1000,
     keys: (process.env.COOKIE_SESSION_KEY_1 && process.env.COOKIE_SESSION_KEY_2) ? [process.env.COOKIE_SESSION_KEY_1, process.env.COOKIE_SESSION_KEY_2] : undefined
