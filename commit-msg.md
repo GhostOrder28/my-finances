a87221514ec2bc71594c40237dba24be98a1ff4e
Ghost Order <kodaichi.kun@gmail.com>
2023-02-07 19:40:27 -0500

    server and axios settings change to production

--- commit-msg.md
+++ commit-msg.md
@@ -1,4 +1,7 @@
-Frontend fixes and improvements.
+Backend.
+* lower case sanitization for auth models added.
+
+Frontend: fixes and improvements.
 * error handling fixed for auth-page.vue.
 * error messages now are colored in red and its size was reduced.
 * error messages alignment fixed when multiple error messages were displayed in sale-form-page.vue. 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,21 @@
 import "dotenv/config";
 // import dotenv from 'dotenv';
 // dotenv.config();
-// import http from "http";
-import path from "path";
-import https from "https";
-import { readFileSync } from "fs";
+import http from "http";
+// import path from "path";
+// import https from "https";
+// import { readFileSync } from "fs";
 
 import app from "./app.js";
 import { mongoConnect } from "./services/mongo.js";
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 
-const server = https.createServer({
-  cert: readFileSync(`${path.resolve()}/src/security/localhost.crt`),
-  key: readFileSync(`${path.resolve()}/src/security/localhost.key`),
-}, app);
+// const server = https.createServer({
+//   cert: readFileSync(`${path.resolve()}/src/security/localhost.crt`),
+//   key: readFileSync(`${path.resolve()}/src/security/localhost.key`),
+// }, app);
 
 const PORT = process.env.PORT || 3001;
 
