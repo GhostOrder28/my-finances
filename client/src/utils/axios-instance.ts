89f3289772de23063a367b4d8fa6c70451009034
GhostOrder28 <jtrvic7@gmail.com>
2024-07-10 13:06:08 -0500

    minor changes

--- client/src/utils/axios-instance.ts
+++ client/src/utils/axios-instance.ts
@@ -3,13 +3,8 @@ import store from '../store/index';
 import router from "@/router";
 
 const options = {
-  // baseURL: 'https://localhost:4000',
-  // baseURL: 'https://192.168.100.3:3001',
   baseURL: '/',
   withCredentials: true,
-  // headers: {
-  //   'Content-Type': 'application/json'
-  // }
 }
 
 const http = axios.create(options);
