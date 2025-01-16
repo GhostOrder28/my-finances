f26246b3a9e57e15cdfe48f501beaffb5bc9b053
GhostOrder28 <jtrvic7@gmail.com>
2025-01-16 17:51:45 -0500

    console.log enabled

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index'
 
-console.log = () => null
+// console.log = () => null
 
 createApp(App)
   .use(router)
